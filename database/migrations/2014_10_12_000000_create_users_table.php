61978d24082c6a7ea8fce520c86e008367b1fb68
Ayberk Ersoy <ayberkersoy@gmail.com>
2018-12-05 11:04:07 +0300

    Add company migration, model, controller and users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->string('status')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
