663012e6cc1379393a04c5d9f1d4ef2e291e7986
Ayberk Ersoy <ayberkersoy@gmail.com>
2018-12-26 14:40:56 +0300

    deployment script

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -9,10 +9,41 @@ class DeployController extends Controller
 {
     public function deploy(Request $request)
     {
-        $root_path = base_path();
-        $process = new Process('cd ' . $root_path . '; ./deploy.sh');
-        $process->run(function ($type, $buffer) {
-            echo $buffer;
-        });
+        /**
+             * GIT DEPLOYMENT SCRIPT
+             *
+             * Used for automatically deploying websites via github or bitbucket, more deets here:
+             * https://gist.github.com/riodw/71f6e2244534deae652962b32b7454e2
+             * How To Use:
+             * https://medium.com/riow/deploy-to-production-server-with-git-using-php-ab69b13f78ad
+             */
+        // The commands
+        $commands = [
+            'cd /home/admin/web/api.maycreator.com/public_html',
+            'echo $PWD',
+            'whoami',
+            'git reset --hard HEAD',
+            'git pull',
+            'git status',
+            'git submodule sync',
+            'git submodule update',
+            'git submodule status',
+        ];
+        // Run the commands for output
+        $output = '';
+        foreach ($commands as $command) {
+            // Run it
+            $tmp = shell_exec($command);
+            // Output
+            $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+            $output .= htmlentities(trim($tmp)) . "\n";
+            echo $output;
+        }
+        // Make it pretty for manual user access (and why not?)
+        // $root_path = base_path();
+        // $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+        // $process->run(function ($type, $buffer) {
+        //     echo $buffer;
+        // });
     }
 }
