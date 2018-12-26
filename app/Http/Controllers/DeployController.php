009bc75eedba2254178282873c34d4ef2285f0ae
Ayberk Ersoy <ayberkersoy@gmail.com>
2018-12-26 15:15:15 +0300

    testing

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -24,7 +24,6 @@ class DeployController extends Controller
             && git status && git submodule sync && git submodule update && git submodule status',
             // 'echo $PWD',
             // 'whoami',
-            // test
             // 'git reset --hard HEAD',
             // 'git pull origin master',
             // 'git status',
