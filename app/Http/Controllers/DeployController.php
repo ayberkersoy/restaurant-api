aea5b0315277d8f81bafa252e3871703fe781cca
Ayberk Ersoy <ayberkersoy@gmail.com>
2018-12-26 13:07:04 +0300

    Update DeployController.php

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -9,18 +9,23 @@ class DeployController extends Controller
 {
   public function deploy(Request $request)
   {
-      dd($request);
-     $githubPayload = $request->getContent();
+//       dd($request);
+//      $githubPayload = $request->getContent();
       
-     $githubHash = $request->header('X-Hub-Signature');
-     $localToken = config('app.deploy_secret');
-     $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
-     if (hash_equals($githubHash, $localHash)) {
-          $root_path = base_path();
-          $process = new Process('cd ' . $root_path . '; ./deploy.sh');
-          $process->run(function ($type, $buffer) {
-              echo $buffer;
-          });
-     }
+//      $githubHash = $request->header('X-Hub-Signature');
+//      $localToken = config('app.deploy_secret');
+//      $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+//      if (hash_equals($githubHash, $localHash)) {
+//           $root_path = base_path();
+//           $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+//           $process->run(function ($type, $buffer) {
+//               echo $buffer;
+//           });
+//      }
+      $root_path = base_path();
+      $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+      $process->run(function ($type, $buffer) {
+          echo $buffer;
+      });
   }
 }
