0e001cf507fff02d64f695922557bbbe201bd61c
Ayberk Ersoy <ayberkersoy@gmail.com>
2018-12-26 14:20:12 +0300

    test deployment #10

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -9,7 +9,6 @@ class DeployController extends Controller
 {
     public function deploy(Request $request)
     {
-        // asdsad
         $root_path = base_path();
         $process = new Process('cd ' . $root_path . '; ./deploy.sh');
         $process->run(function ($type, $buffer) {
