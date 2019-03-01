d0844f5746718603d00a6e1c4b73751fc57f7218
Ayberk Ersoy <ayberkersoy@gmail.com>
2019-03-01 13:36:23 +0300

    Change admin url

--- resources/views/admin/campaigns/index.blade.php
+++ resources/views/admin/campaigns/index.blade.php
@@ -17,7 +17,7 @@
         <div class="box-header with-border">
             <h3 class="box-title">Kampanyalar</h3>
             <div class="box-tools pull-right">
-                <a class="btn btn-success" title="Yeni" href="/admin/campaigns/create"><i class="fa fa-plus"></i> Yeni Ekle</a>
+                <a class="btn btn-success" title="Yeni" href="/campaigns/create"><i class="fa fa-plus"></i> Yeni Ekle</a>
                 <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
                 <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
             </div>
