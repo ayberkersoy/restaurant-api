034df61ac39d1da532d51987985d129b9019e891
Ayberk Ersoy <ayberkersoy@gmail.com>
2019-05-27 12:20:31 +0300

    Change order status on orders index page

--- resources/js/components/orders/IndexOrders.vue
+++ resources/js/components/orders/IndexOrders.vue
@@ -8,6 +8,7 @@
                     <th>Sipariş Sahibi</th>
                     <th>Sipariş Tutarı</th>
                     <th>Sipariş Durumu</th>
+                    <th>Sipariş Durumu</th>
                     <th>Sipariş Notu</th>
                     <th>İşlemler</th>
                 </tr>
@@ -23,6 +24,20 @@
                         <span v-if="order.status === 1" class="label label-warning">Bekliyor</span>
                         <span v-if="order.status === 2" class="label label-primary">Onaylandı</span>
                         <span v-if="order.status === 3" class="label label-success">Gönderildi</span>
+                    </td>
+                    <td class="form-inline">
+                        <div class="form-group">
+                            <select name="status" class="form-control" :id="order.id">
+                                <option value="" selected disabled>Seçiniz</option>
+                                <option value="0">İptal Edildi</option>
+                                <option value="1">Bekliyor</option>
+                                <option value="2">Onaylandı</option>
+                                <option value="3">Gönderildi</option>
+                            </select>
+                        </div>
+                        <a class="btn btn-success" @click.prevent="orderUpdated(order.id)" id="edit" name="edit">
+                            <span class="glyphicon glyphicon-edit"></span>
+                        </a>
                     </td>
                     <td>{{ order.note }}</td>
                     <td>
@@ -46,7 +61,8 @@
         data: function() {
             return {
                 orders: [],
-                id: ''
+                id: '',
+                status: ''
             }
         },
 
@@ -69,6 +85,17 @@
             },
             editPage(id) {
                 window.location = '/orders/' + id + '/show';
+            },
+            orderUpdated(id){
+                const config = {
+                    headers: { 'content-type': 'multipart/form-data' }
+                };
+                let formData = new FormData();
+                let status = document.getElementById(id);
+                formData.append('status', status.options[status.selectedIndex].value);
+                axios.post('/api/orders/' + id + '/updated', formData, config).then(response => {
+                    window.location = '/orders';
+                });
             }
         }
     }
